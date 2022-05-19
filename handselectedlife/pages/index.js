1269db509ed0555d043b204c1b60a0623508d709
syd333 <sydmiranda@icloud.com>
2022-05-18 20:44:26 -0400

    handselected init setup

---
+++ handselectedlife/pages/index.js
@@ -0,0 +1,13 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../styles/Home.module.css'
+
+function HomePage() {
+  return ( 
+  <div>
+    HANDSELECTED LIFE
+  </div>
+  )
+}
+
+export default HomePage;
